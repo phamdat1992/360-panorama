5eb4d4ac564c3477528ac70d41e1560a919c689a
dathtp <phamhuuthanhdat1992@yahoo.com.vn>
2019-10-09 10:02:32 +0700

    add demo 10

--- demo (6-10-2019)/asset/js/main.js
+++ demo (6-10-2019)/asset/js/main.js
@@ -2,8 +2,6 @@ let processor = {
     timerCallback: function() {
         if(this.video.readyState === 4) {
             this.video.currentTime += 1 / this.fps * this.videoSpeed;
-        } else {
-            this.video.currentTime = 0;
         }
 
         if (this.video.duration <= this.video.currentTime) {
@@ -23,7 +21,7 @@ let processor = {
         }.bind(this), 1000/this.fps);
     },
 
-    initVideoEvent: function() {
+    initEvent: function() {
         this.rightButton.addEventListener("click", function() {
             this.videoSpeed = this.defaultSpeed;
         }.bind(this), false);
@@ -34,40 +32,19 @@ let processor = {
     },
 
     doLoad: function() {
-        this.leftButton = document.getElementById("left");
-        this.rightButton = document.getElementById("right")
+        this.leftButton = document.getElementById("button-left");
+        this.rightButton = document.getElementById("button-right")
         this.video = document.getElementById("video");
-        this.canvas = document.getElementById("myCanvas");
+        this.canvas = document.getElementById("canvas");
         this.loading = document.getElementById("loading");
         this.ctx = this.canvas.getContext("2d");
         this.fps = 24;
         this.defaultSpeed = 3;
         this.videoSpeed = 0;
 
-        const mediaSource = new MediaSource();
-        this.video.src = URL.createObjectURL(mediaSource);
-        mediaSource.addEventListener('sourceopen', function(){
-            URL.revokeObjectURL(this.video.src);
-            const sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="vp09.00.10.08"');
-
-            // If video is preloaded already, fetch will return immediately a response
-            // from the browser cache (memory cache). Otherwise, it will perform a
-            // regular network fetch.
-            fetch('./asset/video/clip.mp4', { mode: "no-cors" })
-                .then(response => response.arrayBuffer())
-                .then(data => {
-                    // Append the data into the new sourceBuffer.
-                    sourceBuffer.appendBuffer(data);
-                    this.video.addEventListener("canplaythrough", function(){
-                        this.timerCallback();
-                        this.initVideoEvent();
-                        this.loading.classList.add("hidden-loading");
-                    }.bind(this), { once: true });
-                })
-                .catch(error => {
-                    // TODO: Show "Video is not available" message to user.
-                });
-        }.bind(this), { once: true });
+        this.video.pause();
+        this.initEvent();
+        this.timerCallback();
     },
 
     computeFrame: function() {
