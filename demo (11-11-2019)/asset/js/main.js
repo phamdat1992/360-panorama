2c16b6d1bcf089732b7180704ead4402b57318f8
dathtp <phamhuuthanhdat1992@yahoo.com.vn>
2019-10-25 14:49:24 +0700

    add code

--- demo (11-11-2019)/asset/js/main.js
+++ demo (11-11-2019)/asset/js/main.js
@@ -2,8 +2,8 @@ window.addEventListener('load', onVrViewLoad);
 
 function onVrViewLoad() {
     let vrView = new VRView.Player('#vrview', {
-        image: './asset/img/test_033.jpg',
-        is_stereo: true,
+        image: 'https://api.codetabs.com/v1/proxy?quest=https://refund-form-id-images.s3-ap-southeast-1.amazonaws.com/zalora-office/test_033.jpg',
+        is_stereo: false,
         width: "500",
         height: "500"
     });
