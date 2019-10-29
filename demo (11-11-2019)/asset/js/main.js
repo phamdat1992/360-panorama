31be321008345e7eed98c9b30906ab09f7480af8
dathtp <phamhuuthanhdat1992@yahoo.com.vn>
2019-10-29 19:25:48 +0700

    add file to project

--- demo (11-11-2019)/asset/js/main.js
+++ demo (11-11-2019)/asset/js/main.js
@@ -1,5 +1,130 @@
 let processor = {
 
+    config: {
+        1 :{
+            url: "./asset/img/1.jpg",
+            map: {
+                left: {
+                    id :2,
+                    px: 0,
+                },
+            }
+        },
+        2: {
+            url: "./asset/img/2.jpg",
+            map: {
+                right: {
+                    id:1,
+                    px: 0,
+                },
+                left: {
+                    id:'a3',
+                    px: 0,
+                },
+                up: {
+                    id: 'b3',
+                    px: 0,
+                },
+            }
+        },
+        a3: {
+            url: "./asset/img/3a.jpg",
+            map: {
+                right: {
+                    id: 2,
+                    px: 0,
+                },
+                down: {
+                    id: 'a4',
+                    px: 0,
+                },
+            }
+        },
+        a4: {
+            url: "./asset/img/4a.jpg",
+            map: {
+                up: {
+                    id: 'a3',
+                    px: 0,
+                },
+                down: {
+                    id: 'a5',
+                    px: 0,
+                },
+            }
+        },
+        a5: {
+            url: "./asset/img/5a.jpg",
+            map: {
+                up: {
+                    id: 'a4',
+                    px: 0,
+                }
+            }
+        },
+        b3: {
+            url: "./asset/img/3b.jpg",
+            map: {
+                up: {
+                    id: 'b4',
+                    px: 0,
+                },
+                down: {
+                    id: '2',
+                    px: 0,
+                },
+            }
+        },
+        b4: {
+            url: "./asset/img/4b.jpg",
+            map: {
+                right: {
+                    id: 'b5',
+                    px: 0,
+                },
+                down: {
+                    id: 'b3',
+                    px: 0,
+                },
+            }
+        },
+        b5: {
+            url: "./asset/img/5b.jpg",
+            map: {
+                left: {
+                    id: 'b4',
+                    px: 0,
+                },
+                right: {
+                    id: 'b6',
+                    px: 0,
+                },
+            }
+        },
+        b6: {
+            url: "./asset/img/6b.jpg",
+            map: {
+                left: {
+                    id: 'b5',
+                    px: 0,
+                },
+                right: {
+                    id: 'b7',
+                    px: 0,
+                },
+            }
+        },
+        b7: {
+            url: "./asset/img/7b.jpg",
+            map: {
+                left: {
+                    id: 'b6',
+                    px: 0,
+                },
+            }
+        },
+    },
+
     initEvent: function() {
         this.image.addEventListener("load", function(){
             this.height = this.panorama.offsetHeight * 10.0 / 8.0;
@@ -10,19 +135,19 @@ let processor = {
             this.panorama.style.backgroundPosition = '0px' + ' ' + backgroundPx + 'px';
         }.bind(this), false);
 
-        this.panorama.addEventListener("mousedown", function(){
+        this.content.addEventListener("mousedown", function(){
             this.dragOn = true;
             this.xPos = event.pageX;
             this.yPos = event.pageY;
         }.bind(this), false);
 
-        this.panorama.addEventListener("mouseup", function(){
+        this.content.addEventListener("mouseup", function(){
             this.dragOn = false;
             this.xPos = 0;
             this.yPos = 0;
         }.bind(this), false);
 
-        this.panorama.addEventListener("mousemove", function(){
+        this.content.addEventListener("mousemove", function(){
             if(this.dragOn){
                 let moveX = (this.xPos - event.pageX);
                 let moveY = (this.yPos - event.pageY);
@@ -57,6 +182,7 @@ let processor = {
     doLoad: function() {
         this.panorama = document.getElementById("panorama");
         this.image = document.getElementById("image");
+        this.content = document.getElementById("content");
 
         this.dragOn = false;
         this.xPos = 0;
